2fdd592e6359787fed304437c4c99f245952c5aa
chinwe <renqingwei123@163.com>
2019-03-05 15:11:07 +0800

    try to validate the child form

--- src/app/products/product-edit/product-edit.component.ts
+++ src/app/products/product-edit/product-edit.component.ts
@@ -15,6 +15,7 @@ export class ProductEditComponent implements OnInit {
   errorMessage: string;
 
   product: Product;
+  private dataIsValid: { [key: string]: boolean } = {};
 
   constructor(private productService: ProductService,
               private messageService: MessageService,
@@ -58,6 +59,14 @@ export class ProductEditComponent implements OnInit {
     }
   }
 
+  isValid(path?: string): boolean {
+    this.validate();
+    if(path) {
+      return this.dataIsValid[path];
+    }
+    return (this.dataIsValid && Object.keys(this.dataIsValid).every(d => this.dataIsValid[d] === true ));
+  }
+
   saveProduct(): void {
     if (true === true) {
       if (this.product.id === 0) {
@@ -86,4 +95,27 @@ export class ProductEditComponent implements OnInit {
     // Navigate back to the product list
     this.router.navigate(['/products']);
   }
+
+  validate(): void {
+    // clear the validation object
+    this.dataIsValid = {};
+
+    // 'info' tab
+    if (this.product.productName && this.product.productName.length >= 3 &&
+      this.product.productCode) {
+        this.dataIsValid['info'] = true;
+    } else {
+        this.dataIsValid['info'] = false;
+    }
+
+    // 'tags' tab
+    if (this.product.category && this.product.category.length >= 3) {
+      this.dataIsValid['tags'] = true;
+    } else {
+      this.dataIsValid['tags'] = false;
+    }
+
+  }
+
+
 }
