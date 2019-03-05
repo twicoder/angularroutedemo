66a4aa4c41c53bac1acd46b809565008a70a37a2
chinwe <renqingwei123@163.com>
2019-03-05 14:58:36 +0800

    use child route to construct more complex routes

--- src/app/products/product-edit/product-edit-info.component.ts
+++ src/app/products/product-edit/product-edit-info.component.ts
@@ -11,10 +11,17 @@ export class ProductEditInfoComponent implements OnInit {
   @ViewChild(NgForm) productForm: NgForm;
 
   errorMessage: string;
-  product = { id: 1, productName: 'test', productCode: 'test' };
+  product: Product;
 
   constructor(private route: ActivatedRoute) { }
 
   ngOnInit(): void {
+    this.route.parent.data.subscribe(data => {
+      if (this.productForm) {
+        this.productForm.reset();
+      }
+      this.product = data['resolvedData'].product;
+      this.errorMessage = data['resolvedData'].errorMessage;
+    });
   }
 }
